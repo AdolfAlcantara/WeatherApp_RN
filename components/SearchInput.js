8984acbfd1221d17ab33c1174d54e498d487d363
raulalcantara_96@hotmail.com <raulalcantara_96@hotmail.com>
2018-08-21 18:31:42 -0600

    app finalized

---
+++ components/SearchInput.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { StyleSheet, View, TextInput } from "react-native";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
+  handleChangeText = text => {
+    this.setState({ text:text });
+  };
+
+  handleSubmitEditing = () => {
+    const { onSubmit } = this.props;
+    const { text } = this.state;
+
+    if (!text) return;
+
+    onSubmit(text);
+    this.setState({ text: ''});
+  };
+
+  render() {
+    const { placeholder } = this.props;
+    const { text } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <TextInput
+          autoCorrect={false}
+          value={text}
+          placeholder={placeholder}
+          placeholderTextColor="white"
+          underlineColorAndroid="transparent"
+          style={styles.textInput}
+          clearButtonMode="always"
+          onChangeText={this.handleChangeText}
+          onSubmitEditing={this.handleSubmitEditing}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#666",
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    borderRadius: 5
+  },
+  textInput: {
+    flex: 1,
+    color: "white"
+  }
+});
